7865ce59f0a010c1e1eef988bc70b2b12512bbbe
Brant Strand <bstrand@gmail.com>
2013-09-27 02:49:54 +0000

    Further simplified, using stratified KFold CV iterator

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -20,26 +20,36 @@ from sklearn.cross_validation import KFold
 from sklearn.cross_validation import StratifiedKFold
 
 def main():
+	# Test parameters 
 	min_K = 1
 	max_K = 10
 	k_val=0
-	cv_passes = 5
+	n_folds = 10
 
+	# Load Iris data set
 	iris = datasets.load_iris()
 
-#	np.random.seed()
-#	indices = np.random.permutation(len(iris.data)) #len(irisData) = 150
+	print
+	print "%d-fold cross validation of KNN estimator for values of K from %d-%d" % (n_folds, min_K, max_K)
+	print "-----------------"
 
-	n_folds = 10
+	# TODO: KFold CV iterator isn't working, figure out why
+	#kfold = KFold(len(iris.target), n_folds=n_folds, indices=True)
+
+	# Generate mapping defining different folds of the data set for CV
 	skfold = StratifiedKFold(iris.target, n_folds)
-	kfold = KFold(len(iris.target), n_folds)
 
-	for k_val in range (min_K,max_K):
+	# For various values of K, run the train & test and evaluate nearest neighbor estimator's accuracy
+	for k_val in range (min_K,max_K+1):
 		estimate_KNN_accuracy(k_val, iris.data, iris.target, skfold)
+
+	print
 
 def estimate_KNN_accuracy(k_val, data, target, cv=None):
 	knn = KNeighborsClassifier(k_val)
+	# Train & test the KNN estimator with each fold defined by the provided CV iterator
 	scores = cross_val_score(knn, data, target, cv=cv)
+	# Print summary results for this value of K
 	print("Accuracy for K=%d: %0.2f (+/- %0.2f)" % (k_val, scores.mean(), scores.std() * 2))
 
 main()
