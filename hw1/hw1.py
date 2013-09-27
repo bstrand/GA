aafcb7c878ab09e1a48430e4a12029d46646e869
Brant Strand <bstrand@gmail.com>
2013-09-27 01:35:00 +0000

    Simplified, using sklearn CV methods

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -15,77 +15,31 @@ import numpy as np
 import itertools
 from sklearn import datasets
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cross_validation import cross_val_score
+from sklearn.cross_validation import KFold
+from sklearn.cross_validation import StratifiedKFold
 
 def main():
-	minK = 1
-	maxK = 10
-	xvalN = 10
+	min_K = 1
+	max_K = 10
+	k_val=0
+	cv_passes = 5
 
 	iris = datasets.load_iris()
-	#print "Labels:", repr(np.unique(iris.target_names))
 
-	irisData = iris.data
-	irisClasses = iris.target
-	irisClassNames = iris.target_names
+#	np.random.seed()
+#	indices = np.random.permutation(len(iris.data)) #len(irisData) = 150
 
-	# input: full data set
-	# output: list of equally sized data splits, length N. Each split has train and test list members, spans the full data set, each slice appears once as test
-	# 
-	# random permutation of full data set indices
-	# define N slices
-	# for each slice, create split consisting of test=slice, train=all others
-	# 			test=data[a:b]
-	# 			train=data[0:a] 
-	np.random.seed()
-	indices = np.random.permutation(len(irisData)) #len(irisData) = 150
-	# print "Train slice: %s" % repr(indices[:-10])
-	# print "Test slice: %s" % repr(indices[-10:])
+	n_folds = 10
+	skfold = StratifiedKFold(iris.target, n_folds)
+	kfold = KFold(len(iris.target), n_folds)
 
-	irisData_train = irisData[indices[-50:]]
-	irisClasses_train = irisClasses[indices[-50:]]
+	for k_val in range (min_K,max_K):
+		estimate_KNN_accuracy(k_val, iris.data, iris.target, skfold)
 
-	irisData_test = irisData[indices[:-50]]
-	irisClasses_test = irisClasses[indices[:-50]]
-
-	print "Training set size: %d, Test set size: %d" % (len(irisData_train), len(irisData_test))
-
-
-	kVal=0
-	for kVal in range (1,10):
-		runTrainAndTest(irisData_train,irisClasses_train,irisData_test,irisClasses_test,kVal)
-
-def runTrainAndTest(irisData_train,irisClasses_train,irisData_test,irisClasses_test,kVal=5):
-	knn = KNeighborsClassifier(kVal)
-	knn.fit(irisData_train, irisClasses_train)
-	predictedClasses_test = knn.predict(irisData_test)
-
-	print "Train & test run, K = %d" % kVal
-	#print "%d data points were classified incorrectly" % (countErrors(predictedClasses_test, irisClasses_test))
-
-	# print "Accuracy: %d%%, Test set size: %d, Training set size %d" % (
-	# 	accuracy(predictedClasses_test, irisClasses_test),
-	# 	len(irisData_test),
-	# 	len(irisData_train)
-	# )
-
-	knnScore = knn.score(irisData_test, irisClasses_test)
-	print "KNN score: %.2f" % knnScore
-	print "---"
-
-def countErrors(prediction, key):
-    diff_count = 0
-    for i, j in zip(prediction, key):
-        if i != j:
-            diff_count +=1
-    return diff_count
-
-def accuracy(prediction, key):
-    diff_count = 0
-    for i, j in zip(prediction, key):
-        if i != j:
-            diff_count +=1
-    pctAccuracy = 100 * (1 - (diff_count / len(key)))
-    return pctAccuracy
-
+def estimate_KNN_accuracy(k_val, data, target, cv=None):
+	knn = KNeighborsClassifier(k_val)
+	scores = cross_val_score(knn, data, target, cv=cv)
+	print("Accuracy for K=%d: %0.2f (+/- %0.2f)" % (k_val, scores.mean(), scores.std() * 2))
 
 main()
